7fa7345415cd9a30042924fd0b8a5ed579dead3a
SaiNikhil915 <sainikhilmammula@gmail.com>
2025-04-17 16:47:54 +0530

    done small updates to backend and frontend

--- backend/app.js
+++ backend/app.js
@@ -62,6 +62,6 @@ app.delete("/todos/:todoId", async (req, res) => {
   res.send("Todo Deleted");
 });
 
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+app.listen(3000, '0.0.0.0', () => {
+  console.log("Server running on http://0.0.0.0:3000");
 });
